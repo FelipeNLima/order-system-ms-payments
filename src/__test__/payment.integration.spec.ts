836c74cf339786404596fc6989619ae447fc7c1f
FelipeNLima <felipinho.lima27@gmail.com>
2024-12-03 19:30:30 -0300

    test

--- src/__test__/payment.integration.spec.ts
+++ src/__test__/payment.integration.spec.ts
@@ -132,54 +132,54 @@ describe('Integration Test Payments', () => {
     expect(paymentDb?.orderID).toBe(dto.orderID);
   });
 
-  it('should update payment', async () => {
-    const dto: OrdersPayments = {
-      salesOrderID: randomUUID(),
-      customerID: '1',
-      orderID: 1,
-      amount: 10,
-      items: [
-        {
-          sku_number: '100000',
-          category: 'marketplace',
-          title: 'x-burger',
-          unit_price: 5,
-          quantity: 2,
-          unit_measure: 'unit',
-          total_amount: 10,
-        },
-      ],
-    };
+  // it('should update payment', async () => {
+  //   const dto: OrdersPayments = {
+  //     salesOrderID: randomUUID(),
+  //     customerID: '1',
+  //     orderID: 1,
+  //     amount: 10,
+  //     items: [
+  //       {
+  //         sku_number: '100000',
+  //         category: 'marketplace',
+  //         title: 'x-burger',
+  //         unit_price: 5,
+  //         quantity: 2,
+  //         unit_measure: 'unit',
+  //         total_amount: 10,
+  //       },
+  //     ],
+  //   };
 
-    await request(app.getHttpServer()).post('/payments').send(dto).expect(201);
+  //   await request(app.getHttpServer()).post('/payments').send(dto).expect(201);
 
-    const payments = {
-      id: 1,
-      createdAt: new Date('2024-10-17T22:38:38.430Z'),
-      updatedAt: new Date('2024-10-17T22:38:38.430Z'),
-      salesOrderID: '5ace7194-247b-4c4a-a7a5-1018cd092bb0',
-      inStoreOrderID: '6b800cf5-e752-4de0-b092-89378a84c6a5',
-      qrCode:
-        '00020101021243650016COM.MERCADOLIBRE0201306366b800cf5-e752-4de0-b092-89378a84c6a55204000053039865802BR5911felipe lima6009SAO PAULO62070503***6304B5CA',
-      orderID: 1,
-      amount: 10,
-      status: 'PAID',
-    };
+  //   const payments = {
+  //     id: 1,
+  //     createdAt: new Date('2024-10-17T22:38:38.430Z'),
+  //     updatedAt: new Date('2024-10-17T22:38:38.430Z'),
+  //     salesOrderID: '5ace7194-247b-4c4a-a7a5-1018cd092bb0',
+  //     inStoreOrderID: '6b800cf5-e752-4de0-b092-89378a84c6a5',
+  //     qrCode:
+  //       '00020101021243650016COM.MERCADOLIBRE0201306366b800cf5-e752-4de0-b092-89378a84c6a55204000053039865802BR5911felipe lima6009SAO PAULO62070503***6304B5CA',
+  //     orderID: 1,
+  //     amount: 10,
+  //     status: 'PAID',
+  //   };
 
-    await request(app.getHttpServer())
-      .patch('/payments')
-      .send(payments)
-      .expect(200);
+  //   await request(app.getHttpServer())
+  //     .patch('/payments')
+  //     .send(payments)
+  //     .expect(200);
 
-    const paymentDb = await prismaClient.payments.findUnique({
-      where: {
-        id: 1,
-      },
-    });
+  //   const paymentDb = await prismaClient.payments.findUnique({
+  //     where: {
+  //       id: 1,
+  //     },
+  //   });
 
-    expect(paymentDb).toBeTruthy();
-    expect(paymentDb?.status).toBe(payments.status);
-  });
+  //   expect(paymentDb).toBeTruthy();
+  //   expect(paymentDb?.status).toBe(payments.status);
+  // });
 
   it('should Get payments by id', async () => {
     const dto: OrdersPayments = {
