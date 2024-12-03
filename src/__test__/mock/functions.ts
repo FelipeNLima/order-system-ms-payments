a6d05ccba7ad122bf2178c5e9557f0bb4ceb1236
FelipeNLima <felipinho.lima27@gmail.com>
2024-12-03 19:11:50 -0300

    test

--- src/__test__/mock/functions.ts
+++ src/__test__/mock/functions.ts
@@ -32,10 +32,11 @@ export async function createPayment(
     if (order) {
       const payments = {
         salesOrderID: order?.salesOrderID,
-        qrCode: `00020101021243650016COM.MERCADOLIBRE0201306366b800cf5-e752-4de0-b092-89378a84c6a55204000053039865802BR5911felipe lima6009SAO PAULO62070503***${randomInt(1)}304B5CA`,
+        qrCode:
+          '00020101021243650016COM.MERCADOLIBRE0201306366b800cf5-e752-4de0-b092-89378a84c6a55204000053039865802BR5911felipe lima6009SAO PAULO62070503***0304B5CA',
         inStoreOrderID: '6b800cf5-e752-4de0-b092-89378a84c6a5',
         amount: 10,
-        orderID: order?.orderID,
+        orderID: order?.orderID + randomInt(9),
       };
 
       const response = await prisma.payments.create({
